a9473191df6c137d4065ee82804191b0c8ffe982
thomas <truble@google.com>
2019-09-19 21:13:50 +0000

    better entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,23 +5,22 @@ USER="${USER:-root}"
 source /root/google-cloud-sdk/path.bash.inc
 
 SOURCE_NAME=${SOURCE_PROJECT}:${SOURCE_REGION}:${SOURCE_DATABASE_NAME}
-echo "Starting Cloud SQL Proxy connection to ${SOURCE_NAME}"
+echo "Starting Cloud SQL Proxy for: ${SOURCE_NAME}"
 
-# /cloud_sql_proxy -instances=${SOURCE_NAME}=tcp:3306 -credential_file=/creds/creds.json &
-/cloud_sql_proxy -instances=${SOURCE_NAME}=tcp:3306 &
+/cloud_sql_proxy -instances=${SOURCE_NAME}=tcp:3306 >/dev/null 2>&1 &
 
-sleep 2
+# Attempt connection
 COUNTER=0
-until mysql -u root -h 127.0.0.1 "-p${SOURCE_PASSWORD}" -e '\q'
-do
+until mysql -u root -h 127.0.0.1 "-p${SOURCE_PASSWORD}" -e '\q' >/dev/null 2>&1; do
   if [ $COUNTER -gt 4 ]; then
     echo "Unable to connect SQL proxy."
     exit 1
   fi
-  COUNTER=$(( $COUNTER + 1 ))
-  echo "Waiting for connection . . . "
   sleep 2
+  COUNTER=$(( $COUNTER + 1 ))
+  echo "Waiting for proxy to connect . . . "
 done
 
+echo "Proxy connected and listening at 127.0.0.1:3306"
 
 exec "$@"
